1d05959645698da54e3cc4df7a8913036e284022
Rogelio Garza Rendón <a01571384@tec.mx>
2025-01-23 17:22:08 -0600

    Actividad MAS/GC 1

--- PlanoCubos.py
+++ PlanoCubos.py
@@ -1,6 +1,3 @@
-#Autor: Ivan Olmos Pineda
-
-
 import pygame
 from pygame.locals import *
 
@@ -8,6 +5,8 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+
+import math
 
 # Se carga el archivo de la clase Cubo
 import sys
@@ -22,12 +21,12 @@ ZNEAR=1.0
 ZFAR=900.0
 #Variables para definir la posicion del observador
 #gluLookAt(EYE_X,EYE_Y,EYE_Z,CENTER_X,CENTER_Y,CENTER_Z,UP_X,UP_Y,UP_Z)
-EYE_X=300.0
-EYE_Y=200.0
-EYE_Z=300.0
-CENTER_X=0
-CENTER_Y=0
-CENTER_Z=0
+EYE_X = 300.0
+EYE_Y = 200.0
+EYE_Z = 300.0
+CENTER_X = 0
+CENTER_Y = 0
+CENTER_Z = 0
 UP_X=0
 UP_Y=1
 UP_Z=0
@@ -41,9 +40,21 @@ Z_MAX=500
 #Dimension del plano
 DimBoard = 200
 
-pygame.init()
+#Variables asociados a los objetos de la clase Cubo
+#cubo = Cubo(DimBoard, 1.0)
+cubos = []
+ncubos = 100
 
-cubo = Cubo(DimBoard, 3.0)
+#Variables para el control del observador
+theta = 0.0
+radius = 300
+
+#Arreglo para el manejo de texturas
+textures = []
+filename1 = "textura0.bmp"
+filename2 = "textura3.bmp"
+
+pygame.init()
 
 def Axis():
     glShadeModel(GL_FLAT)
@@ -68,6 +79,20 @@ def Axis():
     glEnd()
     glLineWidth(1.0)
 
+def Texturas(filepath):
+    textures.append(glGenTextures(1))
+    id = len(textures) - 1
+    glBindTexture(GL_TEXTURE_2D, textures[id])
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_CLAMP)
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_CLAMP)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    image = pygame.image.load(filepath).convert()
+    w, h = image.get_rect().size
+    image_data = pygame.image.tostring(image,"RGBA")
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
+    glGenerateMipmap(GL_TEXTURE_2D) 
+
 def Init():
     screen = pygame.display.set_mode(
         (screen_width, screen_height), DOUBLEBUF | OPENGL)
@@ -84,10 +109,25 @@ def Init():
     glEnable(GL_DEPTH_TEST)
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
     
+    Texturas(filename1)
+    Texturas(filename2)
+    
+    for i in range(ncubos):
+        cubos.append(Cubo(DimBoard, 1.0))
 
-def display():  
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    Axis()
+
+#Se mueve al observador circularmente al rededor del plano XZ a una altura fija (EYE_Y)
+def lookat():
+    global EYE_X
+    global EYE_Z
+    global radius
+    EYE_X = radius * (math.cos(math.radians(theta)) + math.sin(math.radians(theta)))
+    EYE_Z = radius * (-math.sin(math.radians(theta)) + math.cos(math.radians(theta)))
+    glLoadIdentity()
+    gluLookAt(EYE_X,EYE_Y,EYE_Z,CENTER_X,CENTER_Y,CENTER_Z,UP_X,UP_Y,UP_Z)
+    #glutPostRedisplay()
+    
+def Plano():
     #Se dibuja el plano gris
     glColor3f(0.3, 0.3, 0.3)
     glBegin(GL_QUADS)
@@ -95,22 +135,63 @@ def display():
     glVertex3d(-DimBoard, 0, DimBoard)
     glVertex3d(DimBoard, 0, DimBoard)
     glVertex3d(DimBoard, 0, -DimBoard)
-    glEnd()
+    glEnd()   
     
-    cubo.drawCube()
-    cubo.update()
+def PlanoTexturizado():
+    #Activate textures
+    glColor3f(1.0,1.0,1.0)
+    glEnable(GL_TEXTURE_2D)
+    #front face
+    glBindTexture(GL_TEXTURE_2D, textures[1])    
+    glBegin(GL_QUADS)
+    glTexCoord2f(0.0, 0.0)
+    glVertex3d(-DimBoard, 0, -DimBoard)
+    glTexCoord2f(0.0, 1.0)
+    glVertex3d(-DimBoard, 0, DimBoard)
+    glTexCoord2f(1.0, 1.0)
+    glVertex3d(DimBoard, 0, DimBoard)
+    glTexCoord2f(1.0, 0.0)
+    glVertex3d(DimBoard, 0, -DimBoard)
+    glEnd()              
+    glDisable(GL_TEXTURE_2D)
     
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    Axis()
+    #Plano()
+    PlanoTexturizado()
+    #Se dibuja cubos
+
+    for obj in cubos:
+        #obj.draw()
+        obj.drawCube(textures,0)
+        obj.update()
     
 done = False
 Init()
 while not done:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+        #if event.type == pygame.QUIT:
+        #    done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                if theta > 359.0:
+                    theta = 0
+                else:
+                    theta += 1.0
+                lookat()
+            if event.key == pygame.K_LEFT:
+                if theta < 1.0:
+                    theta = 360.0
+                else:
+                    theta += -1.0
+                lookat()
+            if event.key == pygame.K_ESCAPE:
+                done = True
 
     display()
 
     pygame.display.flip()
-    pygame.time.wait(10)
+    pygame.time.wait(5)
 
 pygame.quit()
